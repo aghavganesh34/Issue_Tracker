2672210346fb9117e48671f0a1aa530818f2040a
aghavganesh34 <127502651+aghavganesh34@users.noreply.github.com>
2023-06-10 14:31:23 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -11,7 +11,7 @@ db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
 db.once('open',()=>{
     console.log("Connected to Database :: MongoDB ")
 });
-
-module.exports = {
-    MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
-}; 
+module.exports=mongoose;
+// module.exports = {
+//     MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+// }; 
