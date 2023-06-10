55713a02156d74b37a7c621c735a497bf02581c6
aghavganesh34 <127502651+aghavganesh34@users.noreply.github.com>
2023-06-10 14:22:22 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -12,6 +12,6 @@ db.once('open',()=>{
     console.log("Connected to Database :: MongoDB ")
 });
 
-module.exports=module.exports=module.exports = {
+module.exports = {
     MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
 }; 
