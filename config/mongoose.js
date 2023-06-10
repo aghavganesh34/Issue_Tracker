6d8a907e877165bdebb64efa5b78f9cf56aaf3eb
aghavganesh34 <127502651+aghavganesh34@users.noreply.github.com>
2023-06-10 15:20:43 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/db';
 // mongoose.connect('url') 
-mongoose.connect(url);
+const MongoURI="mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+mongoose.connect(MongoURI);
 const db=mongoose.connection;
 
 //If any Error then Getting this Line
